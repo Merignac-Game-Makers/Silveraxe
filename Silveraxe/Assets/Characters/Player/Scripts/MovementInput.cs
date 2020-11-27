9288b27fa74afe73378fa555be8cf500ed45dfe6
Robeen <robert.gras.33@gmail.com>
2020-11-27 17:48:54 +0100

    Déplacement des zones de texte

--- Silveraxe/Assets/Characters/Player/Scripts/MovementInput.cs
+++ Silveraxe/Assets/Characters/Player/Scripts/MovementInput.cs
@@ -33,26 +33,30 @@ public class MovementInput : MonoBehaviour
 		// déplacements au clavier
 		//------------------------
 		screenDirection = ((Vector2)Input.mousePosition - (Vector2)Camera.main.WorldToScreenPoint(playerManager.transform.position)).normalized;
+		if (SceneModeManager.sceneMode != SceneMode.dialogue) {
+			if (screenDirection.y > 0.1 && (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0)) {
+				fTranslation = Input.GetAxis("Vertical") * navAgent.speed * 30;
+				sTranslation = Input.GetAxis("Horizontal") * navAgent.speed * 10;
+				fTranslation *= Time.deltaTime;
+				sTranslation *= Time.deltaTime;
+				move = new Vector3(sTranslation / 2, 0, fTranslation);
+				dest = transform.TransformPoint(move);
+				navAgent.updateRotation = fTranslation > .1;
+				navAgent.SetDestination(dest);
+			} else {
+				fTranslation = 0;
+				sTranslation = 0;
+			}
 
-		if (screenDirection.y > 0.1 && (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0)) {
-			fTranslation = Input.GetAxis("Vertical") * navAgent.speed * 30;
-			sTranslation = Input.GetAxis("Horizontal") * navAgent.speed * 10;
-			fTranslation *= Time.deltaTime;
-			sTranslation *= Time.deltaTime;
-			move = new Vector3(sTranslation / 2, 0, fTranslation);
-			dest = transform.TransformPoint(move);
-			navAgent.updateRotation = fTranslation > .1;
-			navAgent.SetDestination(dest);
-		} else {
-			fTranslation = 0;
-			sTranslation = 0;
 		}
 	}
 
 	private void FixedUpdate() {
 		// rotation en fonction de la direction de la souris
-		if (fTranslation > .1 || Input.GetKey(KeyCode.S)) {
-			transform.Rotate(Vector3.up, screenDirection.x * rotationSensitivity);
+		if (SceneModeManager.sceneMode != SceneMode.dialogue) {
+			if (fTranslation > .1 || Input.GetKey(KeyCode.S)) {
+				transform.Rotate(Vector3.up, screenDirection.x * rotationSensitivity);
+			}
 		}
 	}
 }
