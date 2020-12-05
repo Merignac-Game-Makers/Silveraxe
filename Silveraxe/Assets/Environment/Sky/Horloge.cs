2f5e779cef97d4459eac76c11dab74f68d66077f
Robeen <robert.gras.33@gmail.com>
2020-12-05 15:03:43 +0100

    Le temps passe dans le jeu

---
+++ Silveraxe/Assets/Environment/Sky/Horloge.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Horloge : MonoBehaviour
+{
+    public Animator skyAnimator;
+    public Transform horloge;
+
+    Vector3 rotation = Vector3.zero;
+    public float horlogeValue;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotation.z = -horlogeValue;
+        horloge.rotation = Quaternion.Euler(rotation);
+    }
+}
